3f33c11c483c16358c0d06c21673b1b406167407
haiyo_ <liwensong945@163.com>
2016-05-17 09:55:54 +0800

    Initial Commit

---
+++ ExampleCordova/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  ExampleCordova
+//
+//  Created by v2 on 16/5/17.
+//  Copyright © 2016年 v2. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ ExampleCordova/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  ExampleCordova
+//
+//  Created by v2 on 16/5/17.
+//  Copyright © 2016年 v2. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
